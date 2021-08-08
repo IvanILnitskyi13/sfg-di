762f1dc1b0c3b6ffa9cfa92f5779072d71877b4e
IvanIlnitskyi <ivan.ilnitskyi13@gmail.com>
2021-08-08 14:12:36 +0200

    my-spring-di

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-    public String hello(){
+    public String sayHello(){
         System.out.println("Hello World!!!");
 
         return "Hi Folks!";
